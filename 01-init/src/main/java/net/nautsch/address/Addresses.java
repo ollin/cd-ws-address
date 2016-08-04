12e8ed48d5c765cd380e3078db83efb348d6504a
ollin <oliver.nautsch@gmail.com>
2016-08-04 13:30:51 +0200

    first version

---
+++ 01-init/src/main/java/net/nautsch/address/Addresses.java
@@ -0,0 +1,16 @@
+package net.nautsch.address;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/addresses")
+public class Addresses {
+
+    @RequestMapping(method= RequestMethod.GET)
+    public @ResponseBody String[] all() {
+        return new String[]{"Hello Address!"};
+    }
+}
