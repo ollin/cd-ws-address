b7ed940cd30ccf751b44d00b030ef37cd5b57884
ollin <oliver.nautsch@gmail.com>
2016-08-25 21:13:56 +0200

    initial version

--- 01-init/src/main/java/net/nautsch/address/Addresses.java
+++ 01-init/src/main/java/net/nautsch/address/Addresses.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/addresses")
+@RequestMapping("/")
 public class Addresses {
 
     @RequestMapping(method= RequestMethod.GET)
